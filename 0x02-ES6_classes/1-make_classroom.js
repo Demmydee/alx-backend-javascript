d0ffea13a98272e792a98b00f2d4c53bd4c25d12
Olaoye Ademola <demmydd44@gmail.com>
2024-10-04 10:28:59 -0700

    make some classrooms

--- 0x02-ES6_classes/1-make_classroom.js
+++ 0x02-ES6_classes/1-make_classroom.js
@@ -1,7 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  // Implement the function initializeRooms to return an array of 3 ClassRoom
-  // objects with the size 19, 20, and 34 (in this order)
+  // Call the map method on the array [19, 20, 34], which applies the
+  // provided function to each element in the array.
+  // The map method returns a new array containing the ClassRoom objects
+  // created by the provided function for each element in the original array.
   return [19, 20, 34].map((size) => new ClassRoom(size));
 }
